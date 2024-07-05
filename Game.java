c2fee7888981b8d4f126a1c730f99a0447654eac
Kuldeep Singh <rajputsinghkuldeep@gmail.com>
2024-07-05 10:04:24 +0530

    Alignment Changes Pending

--- Game.java
+++ Game.java
@@ -9,6 +9,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 
 class Game extends JFrame{
+
+    //Creating label for ComputerChoice and Results
+    JLabel computerChoice,result;
+    int compute_Score=0,player_Score=0;
+    //Creating Label for Displaying Score
+    JLabel computeScore,playerScore;
     ImageIcon imageRock, imagePaper, imageScissors;
     public void setup(){
         imageRock = new ImageIcon(new ImageIcon("Images/rock.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
@@ -18,6 +24,26 @@ class Game extends JFrame{
         imageScissors = new ImageIcon(new ImageIcon("Images/scissor.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
 
         //Creating Label
+        computerChoice = new JLabel();
+        computerChoice.setHorizontalTextPosition(JLabel.CENTER);
+        computerChoice.setVerticalTextPosition(JLabel.BOTTOM);
+        computerChoice.setBounds(250,90,100,100);
+
+        result  = new JLabel();
+        result.setFont(new Font("Cambria",Font.BOLD,21));
+        result.setBounds(250,450,100,100);
+
+        JLabel score = new JLabel();
+        score.setBounds(500,0,100,100);
+
+        computeScore = new JLabel();
+        computeScore.setText("Computer : 0");
+        computeScore.setBounds(500,15,100,100);
+
+        playerScore = new JLabel();
+        playerScore.setText("Player : 0");
+        playerScore.setBounds(500,30,100,100);
+
         JLabel labelRock = new JLabel();
         labelRock.setText("Rock");
         labelRock.setIcon(imageRock);
@@ -39,17 +65,96 @@ class Game extends JFrame{
         labelScissors.setVerticalTextPosition(JLabel.BOTTOM);
         labelScissors.setBounds(350,300,100,100);
 
+        //adding mouse listener events
+        labelRock.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e){
+                calculate(labelRock.getText());
+            }            
+        });
+        labelRock.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e){
+                calculate(labelPaper.getText());
+            }            
+        });
+        labelScissors.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e){
+                calculate(labelScissors.getText());
+            }            
+        });
+
         //adding components in Frame
         add(labelRock);
         add(labelPaper);
         add(labelScissors);
+        add(computerChoice);
+        add(result);
+        add(score);
+        add(computeScore);
+        add(playerScore);
         setLayout(null);
     }
 
+    public void calculate(String player){
+        String[] list = {"Rock","Paper","Scissor"};   
+        int random = (int)((Math.random()*10)%3);
+        String computer = list[random];
+        
+        //Displays Computer Choice on screen
+        computerChoice.setText(computer);
+        if(random==0){
+            computerChoice.setIcon(imageRock);
+        }
+        else if(random==1){
+            computerChoice.setIcon(imagePaper);
+        }
+        else{
+            computerChoice.setIcon(imageScissors); 
+        }
+
+        //Logic
+        String res="";
+        if(player.equals(computer)){
+            res = "Draw";
+        }
+        else if(player.equals("Rock")){
+            if(computer.equals("paper")){
+                res = "You Lose!";
+                compute_Score++;
+            }
+            else{
+                res = "You Win!";
+                player_Score++;
+            }
+        }
+        else if(player.equals("Paper")){
+            if(computer.equals("Scissor")){
+                res = "You Lose!";
+                compute_Score++;
+            }
+            else{
+                res = "You Win!";
+                player_Score++;
+            }
+        }
+        else{
+            if(computer.equals("Rock")){
+                res = "You Lose!";
+                compute_Score++;
+            }
+            else{
+                res = "You Win!";
+                player_Score++;
+            }
+        }
+
+        result.setText(res);
+        playerScore.setText("Player : "+playerScore);
+        computeScore.setText("Computer : "+computeScore);
+    }
     public static void main(String[] args) {
         Game g = new Game();
         g.setTitle("ROCK PAPER SCISSOR");
-        g.setBounds(200,200,600,600);
+        g.setBounds(200,200,1000,600);
         g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         g.getContentPane().setBackground(Color.lightGray);
         g.setup();
